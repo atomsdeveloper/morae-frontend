8588c767b60aa8f0b5bef18fc268993cbeae80aa
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-19 19:23:51 -0300

    feat: add attr to state app

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -27,10 +27,12 @@ export interface AppState {
   user: User | null;
   token: string | null;
   reserves: Reserves[];
+  theme: string;
 }
 
 export const initialState: AppState = {
   user: null,
   token: null,
   reserves: [],
+  theme: 'light',
 };
