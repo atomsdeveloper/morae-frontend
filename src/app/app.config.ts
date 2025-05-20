461a9f565c8b780eb12849a0342a72579951c0a2
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-20 19:18:46 -0300

    feat: add req http to provider app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
+// Modules
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
+// Routes
 import { routes } from './app.routes';
+
+// State
 import { provideStore } from '@ngrx/store';
-import { AppReducer } from './app.reducer';
+import { AppReducer } from './state/app.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore({ state: AppReducer }), // Define o estado global.
+    provideHttpClient(),
   ],
 };
